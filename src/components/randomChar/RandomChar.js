5b455c5f709a854094940c7ce84376918f98dae7
Banchee501 <rosstactical373@gmail.com>
2022-10-13 00:55:08 +0300

    add render cardlist, update random char by button and fix image size 'not found'

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -28,7 +28,8 @@ class RandomChar extends Component {
     onCharLoaded = (char) => {
         this.setState({
             char,
-            loading: false
+            loading: false,
+            error: false
         })
     }
 
@@ -73,7 +74,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -85,10 +86,14 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
     const { name, description, thumbnail, homepage, wiki } = char;
+    let imgStyle = { 'objectFit': 'cover' };
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = { 'objectFit': 'contain' }
+    }
 
     return (
-        <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img" />
+        <div className="randomchar__block" >
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle} />
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
@@ -103,7 +108,7 @@ const View = ({ char }) => {
                     </a>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
